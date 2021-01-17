72a9925923a1e8e20a0afe95b9bacbd93e91fa91
Sayanjyoti Das <sayanjyotidasworkmail@gmail.com>
2021-01-17 21:29:09 +0530

    Completed some work

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,19 @@
 var config = require('../config.json');
 var indianStates = require('./data/IndianStates.json');
 var mainMenu = require('./data/mainMenu.json');
+var footerMenu = require('./data/footerMenu.json');
+
+var data = (req, res) => {
+    return {
+        config: config.website,
+        menus: {
+            main: mainMenu,
+            indianStates: indianStates,
+            footer: footerMenu
+        },
+        url: req.url
+    };
+};
 
 module.exports = {
 
@@ -8,32 +21,43 @@ module.exports = {
 
         // Home Page
         app.get('/', function(req, res) {
-            res.render('pages/index', {
-                config: config.website,
-                states: indianStates
-            });
+            res.render('pages/index', data(req, res));
         });
 
         // View Post Page
         app.get('/post/:id', function(req, res) {
-            res.render('pages/post', {
-                config: config.website,
+            res.render('pages/post', Object.assign(data(req, res), {
                 id: req.params.id
-            });
+            }));
+        });
+
+        // Search Page
+        app.get('/search/:q', function(req, res) {
+            res.render('pages/search', Object.assign(data(req, res), {
+                query: req.params.q
+            }));
+        });
+
+        // Posts Page
+        app.get('/posts/:q', function(req, res) {
+            res.render('pages/posts', Object.assign(data(req, res), {
+                query: req.params.q
+            }));
         });
 
         // About Page
         app.get('/about', function(req, res) {
-            res.render('pages/about', {
-                config: config.website
-            });
+            res.render('pages/about', data(req, res));
+        });
+
+        // Contact Us Page
+        app.get('/contact', function(req, res) {
+            res.render('pages/contact', data(req, res));
         });
 
         // Administration Home Page
         admin.get('/', function(req, res) {
-            res.render('index', {
-                config: config.website
-            });
+            res.render('index', data(req, res));
         });
 
     }
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,5 @@
+function _(q) {return document.getElementById(q);}
+
 var xScrollable = {
     prev: function(el) {
         el.parentNode.scrollLeft -= 200;
@@ -6,3 +8,11 @@ var xScrollable = {
         el.parentNode.scrollLeft += 200;
     }
 };
+
+var searchForm = {
+    submit: function () {
+        var q = _('searchInput').value;
+        window.location.href = "/search/" + encodeURIComponent(q);
+        return false;
+    }
+}
