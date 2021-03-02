fa246af190f44d51d3f9c14274e8bf81ed981d7c
Sayanjyoti Das <sayanjyotidasworkmail@gmail.com>
2021-03-02 13:32:50 +0530

    Added webp to supported image upload formats

--- api/helpers.js
+++ api/helpers.js
@@ -1,7 +1,7 @@
 var imageFilter = function(req, file, cb) {
 
     // Accept images only
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF|webp)$/)) {
         req.fileValidationError = 'Only image files are allowed!';
         return cb(new Error('Only image files are allowed!'), false);
     }
