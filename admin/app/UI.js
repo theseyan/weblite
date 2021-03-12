87705f256af111d36d81ed0eb150f74e2ae525a1
Sayanjyoti Das <sayanjyotidasworkmail@gmail.com>
2021-03-12 20:36:42 +0530

    updates to responsiveness

--- admin/app/UI.js
+++ admin/app/UI.js
@@ -1,3 +1,5 @@
+import {Util} from './Core';
+
 export var Notify = (type, text) => {
     alert(text);
 };
@@ -5,4 +7,33 @@ export var Notify = (type, text) => {
 export var Confirm = (title, text, next) => {
     var c = confirm(text);
     next(c);
+};
+
+/**
+ * Page UI Functionality
+*/
+
+var Menu = {
+    open: function(el) {
+        Util._('sidebar').style.transform = "translateX(0)";
+        Util._('fade').style.visibility = "visible";
+        Util._('fade').style.backgroundColor = "#00000030";
+        Util._('fade').onclick = Menu.close;
+
+        Util._('sidebar').addEventListener('click', (evt) => {
+            if(evt.target.className.indexOf("item") != -1) Menu.close();
+        });
+    },
+    close: function(el) {
+        Util._('sidebar').style.transform = "translateX(-100%)";
+        Util._('fade').style.backgroundColor = "transparent";
+        setTimeout(() => {
+            Util._('fade').style.visibility = "hidden";
+        }, 200);
+        Util._('fade').onclick = void(0);
+    }
+};
+
+Util._('UI.MenuButton').onclick = (evt) => {
+    Menu.open(evt.currentTarget);
 };
